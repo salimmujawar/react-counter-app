47a3d7a1066774fdd9d30dd5416bd2c4edb21466
salimmujawar <salim685@gmail.com>
2022-07-07 06:41:55 +0400

    Build a Movies list and Handel delete.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
+import Movies from "./components/movies";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Movies />
   </React.StrictMode>
 );
 
